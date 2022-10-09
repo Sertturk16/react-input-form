e3f8ae520ec3e739d00eba4366d872bd4bb58585
Sertturk16 <88446918+Sertturk16@users.noreply.github.com>
2022-10-10 00:35:26 +0300

    add inline doc to rules.ts

--- src/lib/rules.ts
+++ src/lib/rules.ts
@@ -1,9 +1,16 @@
 import { Rule } from './model'
+/**
+ * @param value string to be validated
+ * @param arr array of rules, each rule contains message and rule
+ * rule: {required: BOOLEAN} || {regex: RegExp} || {min: Number} || {max: Number}
+ * @returns error message if not validated or empty string if validated
+ */
 export const rules = (value: string, arr: Rule[]): any => {
   const required = arr.find(rule => rule.required === true)
   const regex = arr.find(rule => rule.regex)
   const min = arr.find(rule => rule.min)
   const max = arr.find(rule => rule.max)
+
   if (required && !value) return required.message
   else if (regex?.regex && !regex.regex.test(value)) return regex.message
   else if (min?.min && value.length < min.min) return min.message
